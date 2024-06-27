576fbbc6f93b30ac956b88a4abeceb115c2c17b9
EggSaled <adrian.escobar.104183@gmail.com>
2024-06-26 23:39:48 -0700

    feat: added redux state logic to expense and budget forms

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,15 +1,39 @@
+import { FormEvent } from 'react';
+import { useAppSelector, useAppDispatch } from '../app/hooks.ts';
+import { withdraw, deposit } from '../app/budgetSlice.ts';
 import './BudgetForm.css';
 
 export function BudgetForm() {
+  const budget = useAppSelector(state => state.budget.value);
+  const dispatch = useAppDispatch();
+
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    const target = event.target as typeof event.target & {
+      action: { value: "withdraw" | "deposit" },
+      amount: { value: number }
+    };
+
+    const action: string = target.action.value;
+    const amount: number = Number(target.amount.value);
+  
+    if(action === "withdraw"){
+      dispatch(withdraw(amount));
+    } else {
+      dispatch(deposit(amount));
+    }
+  };
+
   return (
-    <form className="budget-form">
+    <form onSubmit={submitHandler} className="budget-form">
       <fieldset>
-        <legend>Are you removing or adding money to your budget?</legend>
+        <legend>Are you removing or adding money to your budget? (Current Budget: {budget})</legend>
         <label htmlFor="withdraw">Withdraw </label>
         <input 
           id="withdraw" 
           type="radio" 
-          name="budget-action" 
+          name="action" 
           value="withdraw" 
           defaultChecked 
         />
@@ -17,12 +41,16 @@ export function BudgetForm() {
         <input 
           id="deposit" 
           type="radio" 
-          name="budget-action" 
+          name="action" 
           value="deposit" 
         />
       </fieldset>
       <label htmlFor="amount">Enter Amount: </label>
-      <input id="amount" type="number" name="amount" />
+      <input 
+        id="amount" 
+        type="number" 
+        name="amount" 
+      />
       <button>Apply Changes</button>
     </form>);
 }
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,22 +1,71 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { useAppDispatch } from "../app/hooks";
+import { add } from "../app/expenseSlice";
+import type { Expense } from '../types/Expense.ts';
 import './ExpenseForm.css';
 
 export function ExpenseForm() {
   const [isRecurring, setIsRecurring] = useState<boolean>(false);
 
+  const dispatch = useAppDispatch();
+
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    // Extracting formdata
+    const target = event.target as typeof event.target & {
+      "expense-name": { value: string },
+      "expense-note": { value: string },
+      "expense-cost": { value: number },
+      "expense-period": { value: Expense["period"] }
+    };
+
+    const name: string = target["expense-name"].value.trim();
+    const note: string = target["expense-note"].value.trim();
+    const cost: number = Number(target["expense-cost"].value);
+    
+    // period can be null if the user doesn't specify the expense as a recurring expense.
+    let period: Expense["period"] = null;
+
+    if( isRecurring ) {
+      // expect a Period
+      period = target["expense-period"].value;
+    }
+
+    // create new Expense, dispatch to expense state
+    const newExpense: Expense = {
+      name,
+      note,
+      cost,
+      isRecurring,
+      period
+    };
+
+    dispatch(add(newExpense));
+  };
+
   return (
-    <form className="expense-form">
+    <form onSubmit={submitHandler} className="expense-form">
       <label htmlFor="expense-name">Name: </label>
       <input 
         type="text" 
         id="expense-name" 
         name="expense-name" 
+        required
+      />
+      <label htmlFor="expense-note">Note(s): </label>
+      <input 
+        type="text" 
+        id="expense-note" 
+        name="expense-note" 
       />
       <label htmlFor="expense-cost">Cost: </label>
       <input 
         type="number" 
         id="expense-cost" 
         name="expense-cost" 
+        step={0.01}
+        required
       />
       <label htmlFor="expense-recurring">
         Will this be a Recurring Expense?
@@ -30,8 +79,8 @@ export function ExpenseForm() {
       { isRecurring ?
         <>
           <label htmlFor="expense-period">Please Select Recurring period: </label> 
-          <select id="expense-period" name="expense-period">
-            <option value={-1}>(select one)</option>
+          <select id="expense-period" name="expense-period" required>
+            <option value="">(select one)</option>
             <option value="daily">Daily</option>
             <option value="weekly">Weekly</option>
             <option value="monthly">Monthly</option>
