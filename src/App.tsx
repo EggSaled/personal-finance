5a6d064ea51acf00f2c0657e329fdcf6b654da61
EggSaled <adrian.escobar.104183@gmail.com>
2024-06-27 22:10:08 -0700

    feat: added primitive routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { ExpenseForm } from './components/ExpenseForm';
+import { BudgetForm } from './components/BudgetForm';
+import { AccountOverview } from './components/AccountOverview';
+import { Header } from './components/header';
+import './App.css';
+import { useAppSelector } from './app/hooks';
 
 function App() {
+  const currentComponent = [ <AccountOverview />, <BudgetForm />, <ExpenseForm /> ];
+
+  const index = useAppSelector(state => state.index.value);
+
+  return (
+    <>
+      <Header />
+      <div className="main-container">
+        { currentComponent[index] }
+      </div>
+    </>);
 }
 
 export default App
