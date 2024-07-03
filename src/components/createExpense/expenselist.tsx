4ae4816d63bc4db33f1d402e501359998d0b3f18
EggSaled <adrian.escobar.104183@gmail.com>
2024-07-03 01:02:39 -0700

    feat: added list to display non-committed expenses

---
+++ src/components/createExpense/expenselist.tsx
@@ -0,0 +1,43 @@
+/* expenselist.tsx - unlike expensetable, expenselist will contain
+ * expenses not yet "committed" to the record; expenses created here
+ * will not show on expense table until they are "committed". */
+
+import { Expense } from "../../types/Expense";
+
+interface props {
+  expenses: Array<Expense>
+  availableBudget: number,
+  onCommitExpense: () => void,
+  onClearExpenses: () => void
+}
+
+export function ExpenseList ({ expenses, availableBudget, onCommitExpense, onClearExpenses }: props){
+
+  return (
+    <div>
+      <h3>New Expenses</h3>
+      <ul>
+        { expenses.length === 0 ? 
+          <em>No expenses recently created.</em> : 
+          expenses.map((e: Expense, i:number) => <li key={i}>{e.name} - {e.cost.toFixed(2)}</li>) 
+        }
+      </ul>
+      <em>Adding these onto your account overview will change your available budget to: ${availableBudget.toFixed(2)}</em>
+      <br />
+      <span>
+        <button 
+          onClick={onCommitExpense} 
+          disabled={expenses.length === 0}
+        >
+          Commit Expenses
+        </button>
+        <button 
+          onClick={onClearExpenses} 
+          disabled={expenses.length === 0}
+        >
+          Clear New Expenses
+        </button>
+      </span>
+    </div>
+  );
+};
