d75b676cae79acd70a859948bde3d170386d6b96
EggSaled <adrian.escobar.104183@gmail.com>
2024-08-12 13:52:07 -0700

    feat: added nanoid assignment to addMany reducer

--- src/app/expenseSlice.ts
+++ src/app/expenseSlice.ts
@@ -2,16 +2,12 @@ import { createSlice, PayloadAction, nanoid } from "@reduxjs/toolkit";
 import type { Expense } from "../types/Expense";
 import type { RootState } from "./store";
 
-/* Using capacity as an id, since we aren't relying on a database
- * for this application. */
 export interface ExpenseState {
-  expenses: Array<Expense>,
-  capacity: number
+  expenses: Array<Expense>
 };
 
 const initialState = {
-  expenses: [],
-  capacity: 0
+  expenses: []
 } as ExpenseState;
 
 export const expenseSlice = createSlice({
@@ -19,14 +15,13 @@ export const expenseSlice = createSlice({
   initialState,
   reducers: {
     remove: (state, action: PayloadAction<Expense>) => {
-      if(state.capacity == 0 || state.expenses.length === 0) return;
+      if(state.expenses.length === 0) return;
 
       state.expenses = state.expenses.filter(e => e.id !== action.payload.id);
     },
     addOne: { 
       reducer(state, action: PayloadAction<Expense>) {
         state.expenses.push(action.payload);
-        state.capacity += 1;
       },
       prepare(expense: Expense) {
         expense.id = nanoid();
@@ -35,12 +30,22 @@ export const expenseSlice = createSlice({
         };
       },
     },
-    addMany: (state, action: PayloadAction<Array<Expense>>) => {
-      action.payload.forEach((element: Expense) => {
-        element.id = state.capacity;
-        state.expenses.push(element);
-        state.capacity += 1;
-      });
+    addMany: { 
+      reducer(state, action: PayloadAction<Array<Expense>>) {
+        action.payload.forEach((element: Expense) => {
+          state.expenses.push(element);
+        });
+      },
+      prepare(expenses: Array<Expense>) {
+        expenses.forEach((e) => {
+          e.id = nanoid();
+          return;
+        });
+
+        return { 
+          payload: expenses 
+        };
+      }
     }
   }
 });
